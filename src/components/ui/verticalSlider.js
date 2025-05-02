c1a26be732b51414a9eb1be89e1ba78e4851625b
Dikiy Perec <hopabsq@mail.ru>
2025-05-02 18:10:36 +0600

    FlashList (vip)

--- src/components/ui/verticalSlider.js
+++ src/components/ui/verticalSlider.js
@@ -1,18 +1,19 @@
 import React, { useState, useRef, useEffect } from "react";
 import { View, Animated, Dimensions } from "react-native";
+import { FlashList } from "@shopify/flash-list";
 import styles from "./styles/verticalSliderStyles";
 
 const VerticalSlider = ({ users, onSlideChange = () => {}, RenderSlideComponent }) => {
 	const [containerHeight, setContainerHeight] = useState(Dimensions.get("window").height);
 	const [activeIndex, setActiveIndex] = useState(0);
 	const scrollY = useRef(new Animated.Value(0)).current;
-	const flatListRef = useRef(null);
+	const listRef = useRef(null);
 
 	const handleLayout = e => {
 		const { height } = e.nativeEvent.layout;
-		if (height > 0 && height !== containerHeight) {
+		if (height && height !== containerHeight) {
 			setContainerHeight(height);
-			flatListRef.current?.scrollToOffset({ offset: 0, animated: false });
+			listRef.current?.scrollToOffset({ offset: 0, animated: false });
 			setActiveIndex(0);
 			onSlideChange(0);
 		}
@@ -27,37 +28,44 @@ const VerticalSlider = ({ users, onSlideChange = () => {}, RenderSlideComponent 
 			}
 		});
 		return () => scrollY.removeListener(id);
-	}, [containerHeight, activeIndex, onSlideChange, scrollY]);
+	}, [containerHeight, activeIndex]);
 
 	const renderItem = ({ item, index }) => {
 		const isActive = index === activeIndex;
 		return (
 			<View style={{ height: containerHeight }}>
-				<RenderSlideComponent user={item.author} posts={item.posts} active={!isActive} />
+				<RenderSlideComponent user={item.author} posts={item.posts} active={isActive} />
 			</View>
 		);
 	};
 
 	return (
 		<View style={styles.outerContainer} onLayout={handleLayout}>
-			<Animated.FlatList
-				ref={flatListRef}
-				data={users}
-				keyExtractor={(_, i) => i.toString()}
-				renderItem={renderItem}
-				showsVerticalScrollIndicator={false}
-				scrollEventThrottle={16}
-				pagingEnabled
-				decelerationRate='fast'
-				snapToInterval={containerHeight}
-				snapToAlignment='start'
-				onScroll={Animated.event([{ nativeEvent: { contentOffset: { y: scrollY } } }], { useNativeDriver: true })}
-				getItemLayout={(_, index) => ({
-					length: containerHeight,
-					offset: containerHeight * index,
-					index,
-				})}
-			/>
+			<Animated.View style={{ flex: 1 }}>
+				<FlashList
+					ref={listRef}
+					data={users}
+					keyExtractor={(_, index) => String(index)}
+					renderItem={renderItem}
+					estimatedItemSize={containerHeight}
+					showsVerticalScrollIndicator={false}
+					initialNumToRender={4}
+					maxToRenderPerBatch={2}
+					pagingEnabled
+					decelerationRate="fast"
+					snapToAlignment="start"
+					snapToInterval={containerHeight}
+					onScroll={Animated.event(
+						[{ nativeEvent: { contentOffset: { y: scrollY } } }],
+						{ useNativeDriver: true }
+					)}
+					getItemLayout={(_, index) => ({
+						length: containerHeight,
+						offset: containerHeight * index,
+						index,
+					})}
+				/>
+			</Animated.View>
 		</View>
 	);
 };
