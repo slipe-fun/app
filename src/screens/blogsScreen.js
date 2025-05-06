291313226baf2c17526885b6869fa52a05449b06
pip64 <76626915+pip64@users.noreply.github.com>
2025-05-06 22:53:07 +0700

    Fixed vertical posts load

--- src/screens/blogsScreen.js
+++ src/screens/blogsScreen.js
@@ -2,17 +2,21 @@ import { Platform, View } from "react-native";
 import { COLORS } from "../constants/theme";
 import VerticalSlider from "../components/ui/verticalSlider";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import UserCard from "../components/common/blogsScreen/userCard";
 import { useFetchUsers } from "../hooks/useFetchUsers";
+import { useState, useEffect } from "react";
 
 const BlogsScreen = () => {
 	const insets = useSafeAreaInsets();
 
 	const { users, handleFetchUsers } = useFetchUsers();
 
-	async function handleSlideChange(currentSlide) {
-		if (currentSlide === Object.keys(users).length - 2) handleFetchUsers();
-	}
+	const [currentSlide, setCurrentSlide] = useState(0);
+
+	const handleSlideChange = async currentSlide => setCurrentSlide(currentSlide);
+
+	useEffect(() => {
+		if (currentSlide === users.length - 2) handleFetchUsers();
+	}, [currentSlide, users, handleFetchUsers]);
 
 	return (
 		<View style={{ flex: 1, paddingTop: Platform.OS === "ios" ? insets.top - 2 : insets.top + 6, backgroundColor: COLORS.black }}>
