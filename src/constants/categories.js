940f03aa9eaed229f414556f9fe1bcc29ddd6282
Dikiy Perec <hopabsq@mail.ru>
2025-06-08 03:20:26 +0600

    🚡|🐾 --- Created categories list with thumbnails (W.I.P)

---
+++ src/constants/categories.js
@@ -0,0 +1,62 @@
+export const categories = [
+    {
+        name: 'Art',
+        thumbnail: require('../../assets/thumbnails/categories/art.webp'),
+        color: '#CB9C6D',
+        secondaryColor: '#754F2A',
+    },
+    {
+        name: 'Games',
+        thumbnail: require('../../assets/thumbnails/categories/games.webp'),
+        color: '#9C937B',
+        secondaryColor: '#494437',
+    },
+    {
+        name: 'Cooking',
+        thumbnail: require('../../assets/thumbnails/categories/cooking.webp'),
+        color: '#D07A32',
+        secondaryColor: '#573314',
+    },
+    {
+        name: 'Fun stuff',
+        thumbnail: require('../../assets/thumbnails/categories/fun.webp'),
+        color: '#E6DBCB',
+        secondaryColor: '#B49464',
+    },
+    {
+        name: 'Science',
+        thumbnail: require('../../assets/thumbnails/categories/science.webp'),
+        color: '#948A7B',
+        secondaryColor: '#413C35',
+    },
+    {
+        name: 'Traveling',
+        thumbnail: require('../../assets/thumbnails/categories/traveling.webp'),
+        color: '#AE5C26',
+        secondaryColor: '#321B0B',
+    },
+    {
+        name: 'Animals',
+        thumbnail: require('../../assets/thumbnails/categories/animals.webp'),
+        color: '#748349',
+        secondaryColor: '#3A4224',
+    },
+    {
+        name: 'Fashion',
+        thumbnail: require('../../assets/thumbnails/categories/fashion.webp'),
+        color: '#DBD2C4',
+        secondaryColor: '#A08964',
+    },
+    {
+        name: 'Other',
+        thumbnail: require('../../assets/thumbnails/categories/other.webp'), 
+        color: '#EAB57B',
+        secondaryColor: '#B0691C',
+    },
+    {
+        name: 'Shitposting',
+        thumbnail: require('../../assets/thumbnails/categories/shitposting.webp'),
+        color: '#777472',
+        secondaryColor: '#2A2928',
+    }
+]
