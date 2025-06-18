9283d46c54164885a423307f531852742b786b5e
pip64 <pip64b@icloud.com>
2025-06-18 23:32:54 +0700

    🐚|📼 --- fix build by aligning Babel plugin 'loose' mode

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,31 @@
 module.exports = {
-    presets: ['module:metro-react-native-babel-preset'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./src'],
-          alias: {
-            "@components": "./src/components",
-            "@constants": "./src/constants",
-            "@hooks": "./src/hooks",
-            "@lib": "./src/lib",
-            "@reducers": "./src/reducers",
-          },
-        },
-      ],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      '@babel/plugin-transform-private-methods',
+      { loose: true },
     ],
-  };
+    [
+      '@babel/plugin-transform-private-property-in-object',
+      { loose: true },
+    ],
+    [
+      '@babel/plugin-transform-class-properties',
+      { loose: true },
+    ],
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@lib': './src/lib',
+          '@reducers': './src/reducers',
+        },
+      },
+    ],
+  ],
+};
