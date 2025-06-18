67a1712c33777a4436d0941ec094bb24c3bfd100
pip64 <pip64b@icloud.com>
2025-06-19 00:09:52 +0700

    🦍|🐐 --- Fixed a lot of warnings

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,10 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['@react-native/babel-preset'],
   plugins: [
     'react-native-reanimated/plugin',
-    [
-      '@babel/plugin-transform-private-methods',
-      { loose: true },
-    ],
-    [
-      '@babel/plugin-transform-private-property-in-object',
-      { loose: true },
-    ],
-    [
-      '@babel/plugin-transform-class-properties',
-      { loose: true },
-    ],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+    ['@babel/plugin-transform-class-properties', { loose: true }],
     [
       'module-resolver',
       {
@@ -25,6 +16,12 @@ module.exports = {
           '@lib': './src/lib',
           '@reducers': './src/reducers',
         },
+      },
+    ],
+    [
+      'transform-inline-environment-variables',
+      {
+        include: ['EXPO_OS'],
       },
     ],
   ],
