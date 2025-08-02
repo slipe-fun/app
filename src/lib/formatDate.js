1768699cf77609b82a2859453f5f431b10080a50
pip64 <pip64b@icloud.com>
2025-08-02 16:11:59 +0700

    🌾|🗼 --- add month localization to notifications screen

--- src/lib/formatDate.js
+++ src/lib/formatDate.js
@@ -1,11 +1,26 @@
+import { useTranslation } from "react-i18next";
+
 export default function (dateStr) {
-    const months = [
-        "января", "февраля", "марта", "апреля", "мая", "июня",
-        "июля", "августа", "сентября", "октября", "ноября", "декабря"
-    ];
+  const { t } = useTranslation();
 
-    const [year, month, day] = dateStr.split("-");
-    const monthName = months[parseInt(month, 10) - 1];
+  const [year, month, day] = dateStr.split("-");
+  const monthIndex = parseInt(month, 10) - 1;
 
-    return `${parseInt(day, 10)} ${monthName} ${year}`;
+  const monthKeys = [
+    "january",
+    "february",
+    "march",
+    "april",
+    "may",
+    "june",
+    "july",
+    "august",
+    "september",
+    "october",
+    "november",
+    "december",
+  ];
+
+  const monthName = t(`months.${monthKeys[monthIndex]}`);
+  return `${parseInt(day, 10)} ${monthName} ${year}`;
 }
